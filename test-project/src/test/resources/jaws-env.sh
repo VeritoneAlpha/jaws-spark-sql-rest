6bd5617b95d93167345d255117f834ada8d0555a
emaorhian <ema.orhian@yahoo.com>
2014-10-08 17:39:28 +0300

    removed test project

--- test-project/src/test/resources/jaws-env.sh
+++
@@ -1,5 +0,0 @@
-export TACHYON_WAREHOUSE_PATH=/sharktables
-export TACHYON_MASTER=tachyon://devbox.local:19998
-export MESOS_NATIVE_LIBRARY=/home/user/mesos-0.19.0/lib/libmesos.so
-export JAVA_OPTS="$JAVA_OPTS  -Djava.library.path=/home/user/mesos-0.19.0/lib/libmesos.so:/home/user/hadoopNativeLibs"
-
