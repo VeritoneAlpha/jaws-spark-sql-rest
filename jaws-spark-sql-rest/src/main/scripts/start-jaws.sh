48f51b6f3f37ce8012426ea695a5942ed29fcfbf
emaorhian <ema.orhian@yahoo.com>
2015-04-22 17:34:35 +0300

    added a configurable logs folder for jaws log4j logs

--- jaws-spark-sql-rest/src/main/scripts/start-jaws.sh
+++ jaws-spark-sql-rest/src/main/scripts/start-jaws.sh
@@ -1,8 +1,13 @@
 dir=`dirname $0`
 parentdir="$(dirname "$dir")"
+logsFolder=$parentdir/logs
+if [ ! -d "$logsFolder" ]; then
+    echo "Creating logs folder"$logsFolder
+    mkdir $logsFolder
+fi
 
 echo "Exporting system variables..."
-. $parentdir/conf/jaws-env.sh
+. $parentdir/conf/jaws-env.sh $logsFolder
 
 echo $TACHYON_WAREHOUSE_PATH
 echo $TACHYON_MASTER
