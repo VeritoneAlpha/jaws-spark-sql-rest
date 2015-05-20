262f906cca99ebebe7e158a4354f62ffeffaadc1
emaorhian <ema.orhian@yahoo.com>
2015-05-20 15:57:49 +0300

    removed the tachyon file browser

--- hdfs-tachyon-file-browser/src/main/scala/customs/Files.scala
+++
@@ -1,8 +0,0 @@
-package customs
-
-import spray.json.DefaultJsonProtocol._
-
-case class Files (parentPath : String, files : Array[File])
-object Files {
-  implicit val filesJson = jsonFormat2(apply)
-}
