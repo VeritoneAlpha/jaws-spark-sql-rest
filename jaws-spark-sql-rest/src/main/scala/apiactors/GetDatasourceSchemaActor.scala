1bd606bed87c62885474ea58c703bbfeac22e23e
Andrei Avramescu <andrei.avramescu@yahoo.com>
2015-05-21 10:45:52 +0300

    updated to xpatterns-spark-parquet dependency from maven-central

--- jaws-spark-sql-rest/src/main/scala/apiactors/GetDatasourceSchemaActor.scala
+++ jaws-spark-sql-rest/src/main/scala/apiactors/GetDatasourceSchemaActor.scala
@@ -6,7 +6,7 @@ import implementation.SchemaSettingsFactory.{ Hdfs, Hive, Parquet, Tachyon }
 import implementation.{ AvroConverter, HiveContextWrapper }
 import messages.GetDatasourceSchemaMessage
 import org.apache.spark.scheduler.HiveUtils
-import org.apache.spark.sql.parquet.ParquetSparkUtility._
+import org.apache.spark.sql.parquet.SparkParquetUtility._
 import server.Configuration
 import scala.concurrent._
 import ExecutionContext.Implicits.global
